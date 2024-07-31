6f376c56be3da2c025acb2b5b0dd158e9c82fca6
MRsabcod <sabeebr97@gmail.com>
2024-07-31 16:23:44 +0500

    students assignments and others updated #5

--- Routes/assignment.js
+++ Routes/assignment.js
@@ -11,52 +11,7 @@ import mongoose from "mongoose";
 import User from "../Models/User.js";
 const assignmentRouter = express.Router();
 
-/**
- * Insert new file.
-const fs = require('fs');
-const {GoogleAuth} = require('google-auth-library');
-const {google} = require('googleapis');
- * @return{obj} file Id
- * */
 
-// async function authorize() {
-//   const jwtClient = new google.auth.JWT(
-//     apikeys.client_email,
-
-//     null,
-//     apikeys.private_key,
-//     SCOPE
-//   );
-// }
-
-// async function uploadBasic() {
-//   // Get credentials and build service
-//   // TODO (developer) - Use appropriate auth mechanism for your app
-
-//   const service = google.drive({
-//     version: "v3",
-//     auth: "43be80cc078b7efa27eb70f399430ca5a0ba7f7a",
-//   });
-//   const requestBody = {
-//     name: "photo.jpg",
-//     fields: "id",
-//   };
-//   const media = {
-//     mimeType: "image/jpeg",
-//     body: fs.createReadStream("files/photo.jpg"),
-//   };
-//   try {
-//     const file = await service.files.create({
-//       requestBody,
-//       media: media,
-//     });
-//     console.log("File Id:", file.data.id);
-//     return file.data.id;
-//   } catch (err) {
-//     console.error('Error listing files:', err);
-//     res.status(500).json({ error: 'Failed to list files' });
-//   }
-// }
 
 assignmentRouter.get("/", async (req, res) => {
   try {
@@ -84,13 +39,16 @@ assignmentRouter.get("/", async (req, res) => {
 assignmentRouter.post(
   "/upload",
   uploads.array("files", 5),
+
   async (req, res) => {
-    const fileNames = req.files
+    let fileNames;
+    if(req.files){
+     fileNames = req.files
       .filter((file) => file.size <= 1000)
-      .map((file) => file.id);
+      .map((file) => file.id);}
     const { title, desc, courseId, deadline } = req.body;
     console.log(fileNames);
-    if (fileNames?.length) {
+    
       const assignment = await Assignment.create({
         title,
         desc,
@@ -105,41 +63,11 @@ assignmentRouter.post(
         return res.status(400).send({ error: "Something went wrong" });
       return res.status(200).send({ assignment: createdAssignment });
     }
-  }
+  
 );
 
 assignmentRouter.get("/assignment/:id", async (req, res) => {
-  // const assignemnt=await Assignment.findById(req.params.id)
-  // const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-  // const __dirname = path.dirname(__filename);
-  // let StreamToText;
-  // let base64String=[];
-  // let fileToStream;
-
-  //  assignemnt.teacherAttachedFileIds?.map(async(fileId)=>{
-  //     const file = await gfs.find({_id:new mongoose.Types.ObjectId(fileId)}).toArray()
-  //    if(file){
-  //  fileToStream=await gfs.openDownloadStreamByName(file[0]?.filename).toArray()
-  //  StreamToText=await fileToStream.toString('base64')
-
-  // // fs.writeFile(`${file[0].filename}`, fileToStream[0], (err) => {
-
-  // //   // In case of a error throw err.
-  // //   if (err) res.send(err)
-  // //     else {
-  // // console.log(fs.readFileSync(`${file[0].filename}`, "utf8"));
-
-  // //     }
-  // // })
-  //    }
-  //    base64String.push(  btoa(
-  //     String.fromCharCode(...new Uint8Array(fileToStream[0]))
-  //   ));
-
-  //   console.log(base64String)
-  // })
-
-  //  res.json({ base64String,assignemnt})
+  
 
   try {
     const assignment = await Assignment.findById(req.params.id);
