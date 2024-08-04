de46158eb32e6d567c598ff1fd5390c875f91e14
MRsabcod <sabeebr97@gmail.com>
2024-08-04 18:50:20 +0500

    studentassignment model updated

--- Models/StudentAssignments.js
+++ Models/StudentAssignments.js
@@ -1,59 +1,66 @@
 import mongoose, { Schema } from "mongoose";
 
-const studentAssignemntSchema=new Schema({
-    studentId:{
-        type:String,
+const studentAssignemntSchema = new Schema({
+    studentId: {
+        type: String,
     },
-    studentName:{
-        type:String,
+    studentName: {
+        type: String,
     },
-    
-    
-    assignments:{
-        type:[
-            {
-                courseId:{
-                    type:String,
-                },
-                assignmentId:{
-                    type:String,
-                },
-                starred: {
-                    type: Boolean,
-                    default: false
-                },
-                studentAttachedLink:{
-                    type:String,
-                },
-                studentAttachedFile:{
-                    type:String,
-                },
-                studentAttachedFileContentType:{
-                    type:String,
-                },
-                studentAttachedFileLinks:[{
-                    type:String,
-                }],
-                grade:{
-                    type:Number,
-                    default:-1,
-                    max:100
-                    
-                }
 
-            }
-        ],
 
+    courses: [{
+        course: {
+            courseId:{type:String},
+            courseName:{type:String},
+
+            courseAssignments:{
+                type:[
+                    {
+                        courseId:{
+                            type:String,
+                        },
+                        assignmentId:{
+                            type:String,
+                        },
+                        starred: {
+                            type: Boolean,
+                            default: false
+                        },
+                        studentAttachedLink:{
+                            type:String,
+                        },
+                        studentAttachedFile:{
+                            type:String,
+                        },
+                        studentAttachedFileContentType:{
+                            type:String,
+                        },
+                        studentAttachedFileLinks:[{
+                            type:String,
+                        }],
+                        grade:{
+                            type:Number,
+                            default:-1,
+                            max:100
+                            
+                        }
+        
+                    }
+                ],
+        
+            },
+        }
+    }],
+    totalPercentage: {
+        type: Number,
+        default: 0,
     },
-    totalPercentage:{
-        type:Number,
-        default : 0,
+    totalGrade: {
+        type: Number,
+        default: 0,
     },
-    totalGrade:{
-        type:Number,
-        default : 0,
-        },
 
 })
-const StudentAssignments=mongoose.model('StudentsAssignments',studentAssignemntSchema)
+const StudentAssignments = mongoose.model('StudentsAssignments', studentAssignemntSchema)
 export default StudentAssignments
